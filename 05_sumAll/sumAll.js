9d5b71b50758ecd05235d32b35044567031722bb
Artem Evseev <Luzifer_Morningstar@mail.ru>
2023-02-14 22:56:06 +0300

    add solution for exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0){
+        return "ERROR";
+    }
+    if (typeof start !== "number" || typeof end !== "number"){
+        return "ERROR";
+    }
+    let lower = start > end ? end : start; 
+    let higher = start > end ? start : end;
+    return (lower + higher) * (higher - lower + 1) / 2;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
