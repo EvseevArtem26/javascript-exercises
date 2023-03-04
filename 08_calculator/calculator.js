8b17dc60838fe567e67a1ab9d0fd1b9e6cc55dc8
Artem Evseev <Luzifer_Morningstar@mail.ru>
2023-03-04 20:34:09 +0300

    add solution for exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((previous, current)=>{
+    return previous + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((previous, current)=>{
+    return previous * current;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(f) {
+  let number = 1;
+	for (let i = 1; i <= f; i++){
+    number *= i;
+  }
+  return number;
 };
 
 // Do not edit below this line
