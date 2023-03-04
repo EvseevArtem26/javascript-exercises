dcc2505bd8c6422e37587524b4215a1630cc1217
Artem Evseev <Luzifer_Morningstar@mail.ru>
2023-03-04 21:07:25 +0300

    add solution for exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = RegExp("[^a-z]", "g");
+    const preparedString = string.toLowerCase().replaceAll(regex, "");
+    return preparedString.split("").reverse().join("") === preparedString;
 };
 
 // Do not edit below this line
